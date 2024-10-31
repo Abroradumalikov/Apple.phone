8d8aacd697dc705200a528bec9f1b99c38ace8c3
Abroradumalikov <abrorabdumalikov1072@gmail.com>
2024-11-01 02:26:41 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,13 +10,36 @@ bar.addEventListener("click", () => {
   menu.classList.toggle("navmenu__box1");
 });
 const izbElements = document.querySelectorAll(".blutooths__img__izb");
+let count = 0; // Hisoblagichni 0 dan boshlaymiz
+
+document.querySelector(".navmenu__right__box__img1").textContent = count;
+
+const clickSound = document.getElementById("clickSound");
 
 izbElements.forEach((izb1) => {
+  izb1.clicked = false;
+
   izb1.addEventListener("click", () => {
+    clickSound.play();
+
     if (izb1.src.endsWith("izb.svg")) {
       izb1.src = "./images/icons/Vector-izb-black.svg";
+
+      if (!izb1.clicked) {
+        count++;
+        izb1.clicked = true;
+      }
     } else {
       izb1.src = "./images/icons/izb.svg";
+
+      if (izb1.clicked) {
+        count--;
+        izb1.clicked = false;
+      }
     }
+
+    document.querySelector(".navmenu__right__box__img1").textContent = count;
+
+    localStorage.setItem("izbCount", count);
   });
 });
